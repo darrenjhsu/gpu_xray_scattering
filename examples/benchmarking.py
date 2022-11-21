5543ee0225b928f52f1a6770ddc1db4101eff749
darrenjhsu <hsudj@ornl.gov>
2022-11-21 11:06:50 -0500

    Sync benchmarking code

--- examples/benchmarking.py
+++ examples/benchmarking.py
@@ -1,4 +1,3 @@
-
 import numpy as np
 from gpu_xray_scattering import XS
 from gpu_xray_scattering.Molecule import Molecule
@@ -30,22 +29,22 @@ S_calc = scatter.scatter(pro, timing=True)
 S_calc = scatter.scatter(pro2, timing=True)
 #print(S_calc[:10])
 
-for i in np.unique(np.logspace(0, 2.9, 50, dtype=int)):
-    pro_coord_stack = np.empty((0, 3))
-    for j in range(i):
-        pro_coord_stack = np.concatenate([pro_coord_stack, pro_coord + j*1])
-    pro_ele_stack = np.repeat(pro_ele, i)
-    pro = Molecule(coordinates=pro_coord_stack, elements=pro_ele_stack)
-    print(f'Num of atoms: {len(pro_ele_stack)}, electrons: {(pro.electrons).sum()}')
-    print('Orientational average 1')
-    S_calc_oa = np.array(scatter_oa.scatter(pro, timing=True))
-    print(S_calc_oa[:10])
-    #print(S_calc[:5], S_calc_oa[:5])
+#for i in np.unique(np.logspace(1, 2.9, 50, dtype=int)):
+#    pro_coord_stack = np.empty((0, 3))
+#    for j in range(i):
+#        pro_coord_stack = np.concatenate([pro_coord_stack, pro_coord + j*1])
+#    pro_ele_stack = np.repeat(pro_ele, i)
+#    pro = Molecule(coordinates=pro_coord_stack, elements=pro_ele_stack)
+#    print(f'Num of atoms: {len(pro_ele_stack)}, electrons: {(pro.electrons).sum()}')
+#    print('Orientational average 1')
+#    S_calc_oa = np.array(scatter_oa.scatter(pro, timing=True))
+#    print(S_calc_oa[:10])
+#    #print(S_calc[:5], S_calc_oa[:5])
 
-for i in np.unique(np.logspace(0, 2.5, 50, dtype=int)):
+for i in np.unique(np.logspace(0, 2.5, 4, dtype=int)):
     pro_coord_stack = np.empty((0, 3))
     for j in range(i):
-        pro_coord_stack = np.concatenate([pro_coord_stack, pro_coord + j*1])
+        pro_coord_stack = np.concatenate([pro_coord_stack, pro_coord + j*50])
     pro_ele_stack = np.repeat(pro_ele, i)
     pro = Molecule(coordinates=pro_coord_stack, elements=pro_ele_stack)
     print(f'Num of atoms: {len(pro_ele_stack)}, electrons: {(pro.electrons).sum()}')
@@ -56,9 +55,8 @@ for i in np.unique(np.logspace(0, 2.5, 50, dtype=int)):
     print(S_calc[:10])
     print(S_calc_oa[:10])
     #print(S_calc[:5], S_calc_oa[:5])
-    rel_diff = np.max(np.abs((S_calc_oa - S_calc) / S_calc))
+    rel_diff_max = np.max(np.abs((S_calc_oa - S_calc) / S_calc))
+    rel_diff_mean = np.mean(np.abs((S_calc_oa - S_calc) / S_calc))
     ref_diff_max_idx = np.argmax(np.abs((S_calc_oa - S_calc) / S_calc))
-    print(f'Relative difference: {rel_diff} at idx {ref_diff_max_idx}: vanilla is {S_calc[ref_diff_max_idx]} and oa is {S_calc_oa[ref_diff_max_idx]}')
+    print(f'Relative difference: mean {rel_diff_mean:.3f}, max {rel_diff_max:.3f} at idx {ref_diff_max_idx}: vanilla is {S_calc[ref_diff_max_idx]} and oa is {S_calc_oa[ref_diff_max_idx]}')
     print()
-
-
